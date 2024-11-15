734f0ae844fe6a128b7516d5b66828b9d79ba2a3
Adelia Jupiter <cjadelia@gmail.com>
2024-11-15 14:40:36 -0500

    completed challenges with passing tests

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -7,47 +7,116 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+            if(vals == null || vals.Length == 0)
+            {
+                return false;
+            }
+
+            foreach(var item in vals)
+            {
+                if (item == false)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+
+            if (numbers == null)
+            {
+                return false;
+            }
+
+            foreach (var num in numbers)
+            {
+                sum += num;
+            }
+            if (sum % 2 != 0)
+            {
+                return true;
+            }
+
+            return false;
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            bool hasUpper = false;
+            bool hasLower = false;
+            bool hasNumber = false;
+            foreach (var letter in password)
+            {
+                if(char.IsUpper(letter)) 
+                { 
+                    hasUpper = true;
+                }
+                if(char.IsLower(letter))
+                {
+                    hasLower = true;
+                }
+                if(char.IsNumber(letter))
+                {
+                    hasNumber = true;
+                }
+            }
+            if(hasUpper && hasLower && hasNumber)
+            {
+                return true;
+            }
+            return false;
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val[0];
         }
-
+        
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return (val[val.Length - 1]);
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            decimal result = 0;
+            if (divisor != 0)
+            {
+                result = dividend / divisor;
+            }
+            return result;
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            return ((nums[nums.Length - 1]) - nums[0]);
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            var odds = new List<int>();
+
+            for(int i = 0; i < 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    odds.Add(i);
+                }
+            }
+
+            return odds.ToArray();
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            for(int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToUpper();
+            }
         }
     }
 }
